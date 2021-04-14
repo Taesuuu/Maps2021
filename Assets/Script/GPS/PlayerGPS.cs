0dea725c4cc1cae46cc5b6187d17105834ae838b
Taesuuu <taesu08@gmail.com>
2021-04-14 20:43:14 +0900

    X , Y Test

--- Assets/Script/GPS/PlayerGPS.cs
+++ Assets/Script/GPS/PlayerGPS.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerGPS : MonoBehaviour
 {
 
-    public static float first_Lat; //최초 위도
-    public static float first_Long; //최초 경도
-    public static float current_Lat; //현재 위도
-    public static float current_Long; //현재 경도
-    public static float f_Lat; //비율 경도
-    public static float f_Long; //비율 경도
+    public Text xText;
+    public Text yText;
+
+    public static float first_Lat; //???? ????
+    public static float first_Long; //???? ????
+    public static float current_Lat; //???? ????
+    public static float current_Long; //???? ????
+    public static float f_Lat; //???? ????
+    public static float f_Long; //???? ????
     
-    public GameObject player; // 플레이어 
+    public GameObject player; // ???????? 
 
     private static WaitForSeconds second;
 
@@ -27,18 +31,18 @@ public class PlayerGPS : MonoBehaviour
 
     IEnumerator Start()
     {
-        // 유저가 GPS 사용중인지 최초 체크
+        // ?????? GPS ?????????? ???? ????
         if (!Input.location.isEnabledByUser)
         {
             Debug.Log("GPS is not enabled");
             yield break;
         }
 
-        //GPS 서비스 시작
+        //GPS ?????? ????
         Input.location.Start();
         Debug.Log("Awaiting initialization");
 
-        //활성화될 때 까지 대기
+        //???????? ?? ???? ????
         int maxWait = 20;
         while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
         {
@@ -46,14 +50,14 @@ public class PlayerGPS : MonoBehaviour
             maxWait -= 1;
         }
 
-        //20초 지날경우 활성화 중단
+        //20?? ???????? ?????? ????
         if (maxWait < 1)
         {
             Debug.Log("Timed out");
             yield break;
         }
 
-        //연결 실패
+        //???? ????
         if (Input.location.status == LocationServiceStatus.Failed)
         {
             Debug.Log("Unable to determine device location");
@@ -62,13 +66,13 @@ public class PlayerGPS : MonoBehaviour
         }
         else
         {
-            //접근 허가됨, 최초 위치 정보 받아오기
+            //???? ??????, ???? ???? ???? ????????
             location = Input.location.lastData;
             first_Lat = location.latitude * 1.0f;
             first_Long = location.longitude * 1.0f;
             gpsStarted = true;
 
-            //현재 위치 갱신
+            //???? ???? ????
             while (gpsStarted)
             {
                 location = Input.location.lastData;
@@ -81,11 +85,11 @@ public class PlayerGPS : MonoBehaviour
 
     public void Map()
     {
-        // 학교 정문 기준 
+        // ???? ???? ???? 
         f_Lat = (float)((current_Lat - 37.48747) * 100000);
         f_Long = (float)(current_Long - 126.81980) * 100000;
     
-        // 과천 이마트 기준
+        // ???? ?????? ????
     //    f_Lat = (float)((current_Lat - 37.42645) * 10000); 
     //    f_Long = (float)(current_Long - 126.99186) * 10000;
 
@@ -105,10 +109,12 @@ public class PlayerGPS : MonoBehaviour
     {
     //    this.transform.position = new Vector3(10, 0, 10);
         this.transform.position = new Vector3(f_Lat, 0, f_Long);
+        xText.text = "X : " + f_Lat.ToString();
+        yText.text = "Y : " + f_Long.ToString();
 
     }
 
-    //위치 서비스 종료
+    //???? ?????? ????
     public static void StopGPS()
     {
         if (Input.location.isEnabledByUser)
